6de5eb1d75c584cc9037d53fee2d2ef5f5b4808b
Joshua Crum <joshua.crum@gmail.com>
2019-07-14 18:59:58 +0000

    Added to applicatioin controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,28 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :sessions 
+    set :session_secret, "pythonsareeverywhere"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
-
+  
+  helpers do 
+    
+    def not_signed_in 
+      if !logged_in? 
+        redirect "Please sign in or sign up."
+      end
+    end
+    
+    def logged_in? 
+      !!session[hunter_id]
+    end
+    
+    def current_hunter 
+      Hunter.find (sessions[:hunter_id])
+    end
+    
 end
