89cf310c9713ba236aa9f7b248e6ba3882bd20ba
Joshua Crum <joshua.crum@gmail.com>
2019-07-16 20:54:55 +0000

    Changed requirements to email from username in hunters table and added timestamps.

--- db/migrate/20190714145111_create_hunters.rb
+++ db/migrate/20190714145111_create_hunters.rb
@@ -1,9 +1,11 @@
 class CreateHunters < ActiveRecord::Migration
   def change
     create_table :hunters do |t|
-      t.string :username
+      t.string :email
       t.string :name 
       t.string :password_digest 
+      
+      t.timestamps null: false
     end
   end
 end
