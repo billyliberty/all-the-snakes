50d63eb9b2cfcada22d78f9cfe820861923684f6
Joshua Crum <joshua.crum@gmail.com>
2019-07-16 23:46:28 +0000

    Update to seed data for testing purposes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 # Hunters:
 
-Hunter.create(email: "pete@email.com", name: "Pete", password: "password")
-Hunter.create(email: "joshua@email.com", name: "Joshua", password: "password")
+Pete = Hunter.create(email: "pete@email.com", name: "Pete", password: "password")
+Joshua = Hunter.create(email: "joshua@email.com", name: "Joshua", password: "password")
 
 # Snakes:
 
 Snake.create(length: 10, weight: 150, monetary_value: 100,)
-Snake.create(length: 10, weight: 150, monetary_value: 100,)
-Snake.create(length: 10, weight: 150, monetary_value: 100,)
-Snake.create(length: 10, weight: 150, monetary_value: 100,)
-Snake.create(length: 10, weight: 150, monetary_value: 100,)
+Snake.create(length: 8, weight: 50, monetary_value: 100,)
+Snake.create(length: 7, weight: 70, monetary_value: 100,)
+Snake.create(length: 16, weight: 190, monetary_value: 100,)
+Snake.create(length: 5, weight: 30, monetary_value: 100,)
 
 #Snake Sacks:
 
-SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
-SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
-SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
+SnakeSack.create(name: "Everglades", date: , hunter_id: joshua.id , capacity: 2, total_value: )
+SnakeSack.create(name: "Florida", date: , hunter_id: joshua.id , capacity: 1 , total_value: )
+SnakeSack.create(name: "Summer", date: , hunter_id: pete.id , capacity: 1, total_value: )
 
 
 
