20663ae6dd1f9b105ec3ad8d5afe2b80516ff401
Joshua Crum <joshua.crum@gmail.com>
2019-07-16 21:08:35 +0000

    Added to seeds.rb file for test data.

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# Hunters:
+
+Hunter.create(email: "pete@email.com", name: "Pete", password: "password")
+Hunter.create(email: "joshua@email.com", name: "Joshua", password: "password")
+
+# Snakes:
+
+Snake.create(length: 10, weight: 150, monetary_value: 100,)
+Snake.create(length: 10, weight: 150, monetary_value: 100,)
+Snake.create(length: 10, weight: 150, monetary_value: 100,)
+Snake.create(length: 10, weight: 150, monetary_value: 100,)
+Snake.create(length: 10, weight: 150, monetary_value: 100,)
+
+#Snake Sacks:
+
+SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
+SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
+SnakeSack.create(name: "", date: , hunter_id: , capacity: , total_value: )
+
+
+
+
+
+
+
