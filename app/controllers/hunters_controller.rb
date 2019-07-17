871bfbd3563dfb9cdfec30bdc95bdc61de3f9b5d
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 21:39:09 +0000

    Revised logout /get.

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -45,8 +45,11 @@ class HuntersController < ApplicationController
   
   get '/logout' do 
     if logged_in?
-    session.destroy 
-    redirect to '/'
+      session.clear 
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
   
 end
