d937963817b8256adae0da091ff72cb760ef26b0
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 18:19:18 +0000

    Added signup to HuntersController.

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,16 +1,25 @@
 class HuntersController < ApplicationController
 
-get 'hunters/:id' do
-  if logged_in?
-    redirect '/snake_sacks'
-  end
+  get 'hunters/:id' do
+   if logged_in?
+    redirect to '/snake_sacks'
+   end
   
   @hunter = Hunter.find_by(params[:id])
   if !@hunter.nil? && @hunter = current_hunter 
-    erb :'hunters/show'
+      erb :'hunters/show'
+    end
   end
   
-end
+  get '/signup' do 
+    if !session[:hunter_id]
+      erb :'/hunters/new'
+    else
+      redirect to '/snakes'
+    end
+  end
+  
+  
 
 
 end
