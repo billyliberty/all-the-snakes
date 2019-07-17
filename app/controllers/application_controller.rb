a23eb1d0991fe9e68c62877665f4523999740bbb
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 01:01:52 +0000

    Revised models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,4 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
-  helpers do 
-    
-    def not_signed_in 
-      if !logged_in? 
-        redirect "Please sign in or sign up."
-      end
-    end
-    
-    def logged_in? 
-      !!session[hunter_id]
-    end
-    
-    def current_hunter 
-      Hunter.find (sessions[:hunter_id])
-    end
-    
 end
