ec5a8e80d0400e956948c1ec664fa6095ce834b3
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 22:07:56 +0000

    Updated SnakesController.

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -1,3 +1,12 @@
 class SnakesController < ApplicationController
   
+  get '/snakes' do 
+    @snakes = Snakes.all 
+    erb :'snakes/index'
+  end
+  
+  get '/snakes/new' do 
+    erb :'snakes/new'
+  end
+  
 end
