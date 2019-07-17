a4313be11aa8f03e4987db0346da9bf3e3689084
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 20:21:00 +0000

    Update to /post for signup.

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -16,8 +16,11 @@ class HuntersController < ApplicationController
   end
   
   post '/signup' do
-    @hunter = Hunter.create(:email => params[:email], :password => params[:password])
-    session[:hunter_id] = @hunter.id
+    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+      @hunter = Hunter.create(params)
+    else
+      redirect to '/signup'
+    end
   end
   
   get '/login' do 
