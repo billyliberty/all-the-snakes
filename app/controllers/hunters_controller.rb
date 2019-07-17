ea965516d653ce408eb4774f9acfcee11b5f054f
Joshua Crum <joshua.crum@gmail.com>
2019-07-17 20:21:47 +0000

    Cleaned up HuntersController.

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,15 +1,4 @@
 class HuntersController < ApplicationController
-
-  get 'hunters/:id' do
-   if logged_in?
-    redirect to '/hunters/show'
-  end
-  
-  @hunter = Hunter.find_by(params[:id])
-  if !@hunter.nil? && @hunter = current_hunter 
-      erb :'hunters/show'
-    end
-  end
   
   get '/signup' do 
     erb :'/hunters/new'
@@ -27,5 +16,4 @@ class HuntersController < ApplicationController
     erb :'hunters/login'
   end
   
-
 end
