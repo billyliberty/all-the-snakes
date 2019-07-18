e7563fc7952b1175b707fe8c183d3e8725865417
Joshua Crum <joshua.crum@gmail.com>
2019-07-18 00:08:19 +0000

    Added to snakes seed test data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ joshua = Hunter.create(email: "joshua@email.com", name: "Joshua", password: "pas
 
 # Snakes:
 
-Snake.create(length: 10, weight: 150, monetary_value: 450, hunter_id: joshua.id )
-Snake.create(length: 8, weight: 50, monetary_value: 400, hunter_id: pete.id)
-Snake.create(length: 7, weight: 70, monetary_value: 300, hunter_id: joshua.id)
-Snake.create(length: 16, weight: 190, monetary_value: 500, hunter_id: pete.id
-Snake.create(length: 5, weight: 30, monetary_value: 100, hunter_id: joshua.id)
+Snake.create(content: "Caught on afternoon in water." length: 10, weight: 150, monetary_value: 450, hunter_id: joshua.id )
+Snake.create(content: "Late night catch on dry land." length: 8, weight: 50, monetary_value: 400, hunter_id: pete.id)
+Snake.create(content: "Second catch of the day." length: 7, weight: 70, monetary_value: 300, hunter_id: joshua.id)
+Snake.create(content: "Why am I doing this? This one almost killed me." length: 16, weight: 190, monetary_value: 500, hunter_id: pete.id
+Snake.create(content: "Little guy lurking in the shadows." length: 5, weight: 30, monetary_value: 100, hunter_id: joshua.id)
 
 
 
