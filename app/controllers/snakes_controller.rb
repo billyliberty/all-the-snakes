6c41e180d8d6d455d0ad6c542e20e59018912254
Joshua Crum <joshua.crum@gmail.com>
2019-07-19 11:43:24 +0000

    Added comments.

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -1,5 +1,7 @@
 class SnakesController < ApplicationController
   
+  # If user is logged in, index of snakes can be viewed. This renders the index page.
+  
   get '/snakes' do 
     if logged_in?
       @snakes = Snake.all 
@@ -9,6 +11,8 @@ class SnakesController < ApplicationController
     end
   end
   
+  # If user is logged in, new snake entry can be created. This renders the creation page. 
+  
   get '/snakes/new' do 
     if logged_in?
       erb :'snakes/new'
@@ -16,6 +20,8 @@ class SnakesController < ApplicationController
       redirect to "/"
     end
   end
+  
+  # If user is logged in, new snake entry can be created. This creates the entry in the database.
   
   post '/snakes' do
     if !logged_in?
@@ -32,6 +38,8 @@ class SnakesController < ApplicationController
     end
   end
   
+  # If user is logged in, user can edit their addition to the database. Users can only edit their own additions. This renders the edit page.
+  
   get '/snakes/:id/edit' do
     find_snake
     if logged_in?
@@ -44,6 +52,8 @@ class SnakesController < ApplicationController
       redirect to "/"
     end
   end
+  
+  # If user is logged in, user can edit their own addition to the database. This updates the database with the edit.
   
   patch '/snakes/:id' do
     find_snake
@@ -60,17 +70,22 @@ class SnakesController < ApplicationController
     end
   end
   
+  # This is a show route for the addition.
+  
   get "/snakes/:id" do
     find_snake
     erb :'/snakes/show'
   end
+  
+  # This permits the deletion of an addition from the database. Users can only delete their own additions.
+
   
   delete '/snakes/:id' do
     find_snake
     @snake = Snake.find(params[:id])
     if @snake.hunter == current_hunter
       @snake.destroy
-      flash[:message] = "Deletion successful"
+      # flash[:message] = "Deletion successful"
       redirect '/'
     else
       redirect '/'
@@ -78,6 +93,8 @@ class SnakesController < ApplicationController
   end
   
   private
+  
+  # This is a helper method for only this controller that locates an addition by its ID and saves it as an instance variable. 
   
     def find_snake
       @snake = Snake.find_by(id: params[:id])
